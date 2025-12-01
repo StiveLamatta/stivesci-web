2f73a98f7631fe1be71b4307010dcca5e0a131e0
Stive Lamatta <stivelamatta@gmail.com>
2025-11-30 19:53:16 -0500

    chore: configure eslint and prettier for husky pre-commit

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  // Esta configuración activa las reglas recomendadas para Astro
+  ...eslintPluginAstro.configs.recommended,
+];
